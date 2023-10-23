7bc4623c6f7ad4bce037687c0d286145e76aa291
damage <damaguk@mail.ru>
2023-10-23 09:46:44 +0300

    #test

--- src/components/test333.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const test333 = () => {
-  return <div>test333</div>;
-};
-
-export default test333;
--- src/components/testing1.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const testing1 = () => {
-  return <div>test333</div>;
-};
-
-export default test333;
